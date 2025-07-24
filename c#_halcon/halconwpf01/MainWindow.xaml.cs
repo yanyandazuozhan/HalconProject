6bfa1733eb238f1c8d509b5610bc569a99bee77d
yanyandazuozhan <996889964@qq.com>
2025-07-24 22:47:10 +0800

    添加了一个wpf调用halcon算子的demo

---
+++ c#_halcon/halconwpf01/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HalconDotNet;
+
+namespace halconwpf01
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var image = new HImage();
+            image.ReadImage("C:/Users/Administrator/Desktop/文档管理/haikang02.png");
+            image.GetImageSize(out int width, out var height);
+            Hwin.HalconWindow.SetPart(0, 0, height, width);
+            image.DispImage(Hwin.HalconWindow);
+        }
+    }
+}
