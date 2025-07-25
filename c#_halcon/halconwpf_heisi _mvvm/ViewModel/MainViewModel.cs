2d4058223c31b962d7e1243fa9092838ab60629d
yanyandazuozhan <996889964@qq.com>
2025-07-25 15:05:41 +0800

    阈值提取demo添加可变阈值提取功能

--- c#_halcon/halconwpf_heisi _mvvm/ViewModel/MainViewModel.cs
+++ c#_halcon/halconwpf_heisi _mvvm/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,42 @@ namespace halconwpf01.ViewModel
         HObject ho_Heisi = new HObject();
         HObject ho_grayimg = new HObject();
         HObject ho_Regions = new HObject();
+        private string imgpath;
         public DelegateCommand initCommand { get; set; }
         public DelegateCommand RgbToGrayCommand {  get; set; }
         public DelegateCommand ThreSholdCommand {  get; set; }
+        private string _yuzhileft;
+        public string yuzhileft
+        {
+            get
+            {
+                return _yuzhileft;
+            }
+            set
+            {
+                _yuzhileft=value;
+                OnPropertyChanged("yuzhileft");
+            }
+        }
+
+        private string _yuzhiright;
+        public string yuzhiright
+        {
+            get
+            {
+                return _yuzhiright;
+            }
+            set
+            {
+                _yuzhiright = value;
+                OnPropertyChanged("yuzhiright");
+            }
+        }
         public MainViewModel()
         {
+            imgpath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "img", "零件图片.png");
+            yuzhileft =4.ToString();
+            yuzhiright=110.ToString();  
             initCommand = new DelegateCommand(initMethod);
             RgbToGrayCommand = new DelegateCommand(RgbToGrayMethod);
             ThreSholdCommand = new DelegateCommand(ThreSholdMethod);
@@ -27,7 +59,7 @@ namespace halconwpf01.ViewModel
 
         private void initMethod()
         {
-            HOperatorSet.ReadImage(out ho_Heisi, "D:/Desktop/文档记录/heisi.png");
+            HOperatorSet.ReadImage(out ho_Heisi, imgpath);
             HOperatorSet.GetImageSize(ho_Heisi, out var width, out var height);
             halconControl.HalconWindow.SetPart(0, 0, height.D, width.D);
             halconControl.HalconWindow.DispObj(ho_Heisi);
@@ -39,8 +71,33 @@ namespace halconwpf01.ViewModel
         }
         private void ThreSholdMethod()
         {
-            HOperatorSet.Threshold(ho_grayimg, out ho_Regions, 4, 110);
-            halconControl.HalconWindow.DispObj(ho_Regions);
+            if (!string.IsNullOrEmpty(yuzhileft) && !string.IsNullOrEmpty(yuzhiright))
+            {
+                try
+                {
+                    int left = int.Parse(yuzhileft);
+                    int right = int.Parse(yuzhiright);
+                    HOperatorSet.ReadImage(out ho_Heisi, imgpath);
+                    HOperatorSet.GetImageSize(ho_Heisi, out var width, out var height);
+                    halconControl.HalconWindow.SetPart(0, 0, height.D, width.D);
+                    halconControl.HalconWindow.DispObj(ho_Heisi);
+
+                    HOperatorSet.Rgb1ToGray(ho_Heisi, out ho_grayimg);
+                    halconControl.HalconWindow.DispObj(ho_grayimg);
+
+                    HOperatorSet.Threshold(ho_grayimg, out ho_Regions, left, right);
+                    halconControl.HalconWindow.DispObj(ho_Regions);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("阈值不能为空");
+            }
+           
         }
     }
 }
